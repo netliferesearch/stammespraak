de56618b2bbe51b37416a223506f92290f246ad2
kmelve <knut@netliferesearch.com>
2017-03-21 09:10:22 +0100

    more quotes, random

--- src/index.js
+++ src/index.js
@@ -2,13 +2,69 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const ordliste = [
+    {
+        word: 'å hapse',
+        definition: '(omniverb): å utføre oppgaver som gagner bedriften, brukes som oftest i imperativ.'
+    },
+    {
+        word: 'fryktfat',
+        definition: '(subst.): Samlingen med bekymringer i forkant og underveis i et prosjekt.'
+    },
+    {
+        word: 'å spurve',
+        definition: 'verb, overført betyd.) ; forsiktig slippe en drittoppgave i hodet på noen andre, som ikke merker det før det er for sent.'
+    },
+    {
+        word: 'Ordlighet',
+        definition: 'At det føles som et begrep som betyr noe selv om du ikke nødvendigvis vet hva det betyr.'
+    },
+    {
+        word: 'Clever-dickness',
+        definition: 'når noen prøver så hardt å være morsom eller flink at andre responderer med å rante.'
+    },
+    {
+        word: 'Hverdagsgenialitet',
+        definition: 'Verdi, målsetning og fokus til Netlife Research Bergen.'
+    },
+    {
+        word: 'Tazzen',
+        definition: '3d-printeren til Netlife Research Bergen. Høyt elsket, hyppig brukt.'
+    },
+    {
+        word: 'Birdie',
+        definition: 'Kjell-Mortens ondsinnede bot. Konseptet Birdie oppsto en dag der TV-serien «Bør de gifte seg?» ble diskutert i lunsjen. Bør de ble til Birdie, og Kjell-Morten kodet opp boten med avansert AI som sikrer at du alltid får svar om at «Nei, det bør de ikke» hvis du håpet på et ja og vise-versa..'
+    },
+    {
+        word: 'Purreløk',
+        definition: 'Når vi sender en purreløk til noen, sender vi en purring. Men purreløk er artigere.'
+    },
+    {
+        word: 'Knutset',
+        definition: 'Å bli fortalt at det du deler allerede er delt av Knut for lenge siden. Brukt i en setning: «Jeg droppet å dele det, i frykt for å bli knutset.»'
+    },
+    {
+        word: 'Rennesteinsfrukt',
+        definition: 'Frukt som er så lavthengende at den ligger i rennesteinen.'
+    },
+    {
+        word: 'bergensning',
+        definition: 'Oslo-kontoret foreslår noe som er gjort for lengst i vest.'
+    },
+    {
+        word: 'Dongelrygg',
+        definition: 'Det du får etter å ha bært rundt på for mange dongler.'
+    }
+]
+const random = Math.floor(Math.random()*ordliste.length)
 ReactDOM.render((
     <div className='wrapper'>
         <div className='container'>
-            <h1>å hapse</h1>
-            <p>(omniverb): å utføre oppgaver som gagner bedriften, brukes som oftest i imperativ.</p>
-            
+            <h1>{ordliste[random].word}</h1>
+            <p>{ordliste[random].definition}</p>
         <div className="cta"><small>Om du liker dette stammespråket <a href="https://www.netliferesearch.com/jobb">kan du kanskje jobbe hos oss</a>?</small></div>
         </div>
     </div>
 ), document.getElementById('root'))
+
+
