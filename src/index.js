92604dcf2f77c8020c312aac5b6577a629552438
kmelve <knut@netliferesearch.com>
2017-03-22 18:22:40 +0100

    styling and lock

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log(process.env.REACT_APP_GRAPHQL_ENDPOINT)
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
@@ -11,9 +10,8 @@ import 'tachyons'
 import './index.css'
 
 import ListPage from './components/ListPage'
-import TestPage from './components/TestPage'
+import DetailPage from './components/DetailPage'
 import CreatePage from './components/CreatePage'
-import Post from './components/Post'
 
 
 const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT })
@@ -21,22 +19,15 @@ const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_GRA
 const client = new ApolloClient({ networkInterface })
 
 ReactDOM.render((
-  <div className="wrapper">
-  <ApolloProvider client={client}>
-    <Router history={browserHistory}>
-      <Route path='/' component={ListPage}>
-            <Route path='create' component={CreatePage} />
-            {/*<Route path='post/:id' component={Post} />*/}
-          </Route>
-        </Router>
-  </ApolloProvider>
+  <div className='wrapper'>
+    <ApolloProvider client={client}>
+      <Router history={browserHistory}>
+        <Route path='/' component={ListPage} />
+        <Route path='create' component={CreatePage} />
+        <Route path='/word/:id' component={DetailPage} />
+      </Router>
+    </ApolloProvider>
   </div>
   ),
   document.getElementById('root')
 )
-/*
-const routerTest = (<Router>
-      <Route exact path='/' component={ListPage} />
-      <Route path='create' component={CreatePage} />
-      <Route path='post/:id' component={DetailPage} />
-    </Router>)*/
