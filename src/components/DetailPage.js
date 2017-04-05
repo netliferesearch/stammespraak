8e72ffc7bac30f5b1783fec5f1578e3cfe8f4e69
Marius Hauken <marius.hauken@gmail.com>
2017-04-05 10:03:13 +0200

    Added metadata to details-page

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react'
+import { Helmet } from 'react-helmet'
 import { Link } from 'react-router'
 import Footer from './Footer'
 import Nav from './Nav'
@@ -16,6 +17,18 @@ const DetailPage = (props) => {
 
   return (
     <div className='container'>
+      <Helmet>
+        <title>{`${word} - Stammespråk`}</title>
+        <link rel='canonical' href={`https://www.stammespråk.no/${word}`} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" name="twitter:title" content={`${word} - Stammespråk`}/>
+        <meta property="og:description" name="twitter:description" content={definition}/>
+        <meta property="og:site_name" content="Stammespråk – av Netlife"/>
+        <meta property="og:url" content={`https://www.stammespråk.no/${word}`}/>
+        <meta property="article:author" content="Netlife Research"/>
+        <meta name="twitter:label1" value={word} />
+        <meta name="twitter:data1" value={definition} />
+      </Helmet>
       <h1 className='no-underline'>{word}</h1>
       <p>{definition}</p>
       <Link to='/'><Nav /></Link>
