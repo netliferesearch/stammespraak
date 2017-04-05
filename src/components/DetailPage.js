9ed0f38dfc27fe54f5ea6d5b21fc0c79c3c807b9
kmelve <knut@netliferesearch.com>
2017-04-05 11:33:45 +0200

    use Container styled component

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,13 +1,16 @@
 import React, {PropTypes} from 'react'
 import { Link } from 'react-router'
+import { Helmet } from 'react-helmet'
+
 import Footer from './Footer'
 import Nav from './Nav'
+import Container from '../primitives/Container'
 
 const DetailPage = (props) => {
   const {
     posts,
     params: {
-      word: postWord,
+      word: postWord
     } = {}
   } = props
 
@@ -15,12 +18,22 @@ const DetailPage = (props) => {
   const { word, definition } = post
 
   return (
-    <div className='container'>
+    <Container>
+    <Helmet>
+        <title>Stammespråk – av Netlife</title>
+        <link rel='canonical' href='https://www.stammespråk.no/' />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={`Stammespråk - ${word}`}/>
+        <meta property="og:site_name" content="Stammespråk – av Netlife"/>
+        <meta property="og:url" content={`https://www.stammesprak.no/${encodeURI(word)}`} />
+        <meta property="og:image" content="https://www.stammesprak.no/pointer.jpg"/>
+        <meta property="article:author" content="Netlife Research"/>
+      </Helmet>
       <h1 className='no-underline'>{word}</h1>
       <p>{definition}</p>
       <Link to='/'><Nav /></Link>
       <Footer />
-    </div>
+    </Container>
   )
 }
 
