2e9060d9dc5eb91462a39c34e4dde69624345e1f
kmelve <knut@netliferesearch.com>
2017-04-24 10:08:22 +0200

    link to everything page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router'
 
 const Footer = (props) => {
   return (
     <div className="cta">
       <small>
         Om du liker dette stammespråket <a href="https://www.netliferesearch.com/jobb">kan du kanskje jobbe hos oss</a>?
-      </small>
+       – <Link to="/alle">se alt stammespråket</Link>
+       </small>
     </div>
   );
 }
