0f7ba0f07ee666dd5746cf0bf061962eca5eadc2
kmelve <knut@netliferesearch.com>
2017-04-28 18:51:07 +0200

    add user to update, report errors

--- wt-stammespraak.js
+++ wt-stammespraak.js
@@ -1,16 +1,19 @@
 require('isomorphic-fetch')
 
 module.exports = (context, cb) => {
+  console.log(context.body)
+  const user = context.body['user_name']
   const text = context.body.text.split(';')
   const definition = text[1]
-  const word = text[0]
+  const word = text[0].toLowerCase()
   const token = `Bearer ${context.secrets.token}`
 
   const query = `mutation {
-    createEntries(definition: "${definition}" word: "${word}") {
+    createEntries(definition: "${definition}" word: "${word}" user: "${user}") {
       id
       word
       definition
+      user
     }
   }`
 
@@ -28,7 +31,11 @@ module.exports = (context, cb) => {
   })
   .then((data) => {
     console.log(data)
-    cb(null, `La til ordet *${word}* med definsjonen «${definition}» på <https://www.stammesprak.no/${encodeURI(word)}|www.stammesprak.no> :tada:`)
+    if (!data.data.createEntries) {
+      cb(null, `Noe gikk galt: ${data.errors.map(error => error.message).join('; ')}`)
+    } else {
+      cb(null, `<@${user}> la til ordet *${word}* med definsjonen «${definition}» på <https://www.stammesprak.no/${encodeURI(word)}|www.stammesprak.no> :tada:`)
+    }
   }).catch(error => {
     console.log(error)
     cb(null, `Noko gjekk gale ${error}`)
